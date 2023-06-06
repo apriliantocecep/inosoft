f0070cb7c181b55addeaf767e919ae3d904efa55
Cecep Aprilianto <cecepaprilianto@gmail.com>
2023-06-06 14:12:14 +0700

    setup auth routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Public
+Route::post('/login', [\App\Http\Controllers\API\AuthController::class, 'login']);
+Route::post('/register', [\App\Http\Controllers\API\AuthController::class, 'register']);
